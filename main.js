b82bc93f17e2b6b4a7b6609b3b57d2f8d6ca255a
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-04-08 19:55:02 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+var getRockLaunch1 = document.getElementById("rockLaunch1");
+var rockLaunch1 = function(){
+getRockLaunch1.textContent = "";
+};
+getRockLaunch1.addEventListener("click", rockLaunch1);
