7315c5f3a567329bd5643d687e2c57816e42f333
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-04-12 17:06:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var getRockLaunch1 = document.getElementById("rockLaunch1");
+var getMoney = document.getElementById("money");
+var money = 0;
 var rockLaunch1 = function(){
-  var money = 0;
+  money = 0;
   var fuel = 100;
   if (fuel > 0 {
       fuel -= 1;
@@ -8,4 +10,5 @@ var rockLaunch1 = function(){
       }
 getRockLaunch1.textContent = "Test";
 };
+
 getRockLaunch1.addEventListener("click", rockLaunch1);
