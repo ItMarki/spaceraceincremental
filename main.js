fd4d1383309cb952adc9d0c1f97a5e79c5462b1f
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-04-12 16:59:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var getRockLaunch1 = document.getElementById("rockLaunch1");
 var rockLaunch1 = function(){
+  var money = 0;
+  var fuel = 100;
+  if (fuel > 0 {
+      fuel -= 1;
+      money += 10
+      }
 getRockLaunch1.textContent = "Rocket launched. Generating currency...";
 };
 getRockLaunch1.addEventListener("click", rockLaunch1);
