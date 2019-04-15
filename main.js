388463f5114f27ecb909412a692dd367aae65334
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-04-15 17:46:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 
 var game = {
   money:0,
-  fuel:100
-  
+  fuel:100,
+  fuelCost:3
 };
 function rockLaunch1() {
   game.money += game.fuel*10
   game.fuel = 0;
   document.getElementById("money").innerHTML = game.money;
+  document.getElementById("fuel").innerHTML = game.fuel;
+};
+function buyFuel() {
+  game.money -= game.fuelCost*100
+  game.fuel =100
+  document.getElementById("money").innerHTML = game.money;
+  document.getElementById("fuel").innerHTML = game.fuel;
 };
 document.getElementById("money").innerHTML = game.money;
 
