f30660bc0d7b8cf8016a28fc4a15492521532502
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-04-16 15:00:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function buyFuel(fuelBought) {
   if (game.money >=game.fuelCost*fuelBought) {
   game.money -= game.fuelCost*fuelBought
   game.fuel += fuelBought
-  game.fuelCost += 0.001 * fuelBought;
+  game.fuelCost = game.fuelCost*(1.0001^fuelBought)
   }
   document.getElementById("money").innerHTML = game.money;
   document.getElementById("fuel").innerHTML = game.fuel;
